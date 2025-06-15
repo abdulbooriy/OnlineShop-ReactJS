6a69943b49292421c20b2b13f6c0ba4ec2130d2b
Abdulboriy <abdulboriy_mahamatjanov@gmail.com>
2025-06-15 15:45:46 +0600

    some bugas debuged

--- src/pages/products/service/mutation/useCreateProducts.ts
+++ src/pages/products/service/mutation/useCreateProducts.ts
@@ -1,11 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import { request } from "../../../../config/request";
 import toast from "react-hot-toast";
-import type { productsList } from "../../types";
+import type { Product } from "../../types";
 
 export const useCreateProducts = () => {
   return useMutation({
-    mutationFn: (data: productsList) =>
+    mutationFn: (data: Product) =>
       request
         .post("/products", data)
         .then((res) => res.data)
